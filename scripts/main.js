814759266e238c8c064278910b804ad800fe4ac8
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-08-12 19:28:31 -0500

    .

--- scripts/main.js
+++ scripts/main.js
@@ -40,7 +40,19 @@ function timeOut() {
     setTimeout(unHide, 3000)
 }
 timeOut();
+// Loading js
+(() => {
+  const $triangles = document.querySelectorAll(".triangle");
+  const template = `<svg class="triangle-svg" viewBox="0 0 140 141">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <polygon class="triangle-polygon"  points="70 6 136 138 4 138"></polygon>
+    </g>
+  </svg>`;
 
+  Array.prototype.forEach.call($triangles, ($triangle, index) => {
+    $triangle.innerHTML = template;
+  });
+})();
 // Set a variable for our button element.
 const scrollToTopButton = document.getElementById("js-top");
 
