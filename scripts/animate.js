cac96624a2fe3e2214340508bc820eaa10b67aae
Ricky Rivas <rickyrivas918@gmail.com>
2021-08-16 18:52:09 -0500

    .

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,32 +1,26 @@
-barba.init({
-    prefetchIgnore: '/home/',
+// barba.init({
+//     prefetchIgnore: '/home/',
 
-    transitions: [{
-        name: 'opacity-transition',
-        leave(data) {
-            return gsap.to(data.current.container, {
-                opacity: 0,
-            });
-        },
-        enter(data) {
-            return gsap.from(data.next.container, {
-                opacity: 0
-            });
-        }
-    }]
-});
+//     transitions: [{
+//         name: 'opacity-transition',
+//         leave(data) {
+//             return gsap.to(data.current.container, {
+//                 opacity: 0,
+//             });
+//         },
+//         enter(data) {
+//             return gsap.from(data.next.container, {
+//                 opacity: 0
+//             });
+//         }
+//     }]
+// });
 
-let rule = CSSRulePlugin.getRule(".landing-container h1 span::after");
+// var tl = gsap.timeline();
 
-var tl = gsap.timeline({
-    defaults: {
-        duration: 1
-    }
-})
-
-tl.from('.anim1', {
-    y: -50,
-    stagger: 0.6,
-    opacity: 0,
-    delay: 2,
-})
+// tl.from('.anim1', {
+//     y: -50,
+//     stagger: 0.6,
+//     opacity: 0,
+//     delay: 2,
+// })
